4a8605eb9d2b6404f74ae17ef8a1e48d6926a640
Albert <albert_nguyen@ucsb.edu>
2025-10-06 21:33:28 -0700

    code

---
+++ cs32/lab00/helloWorld.cpp
@@ -0,0 +1,9 @@
+//Edited By: Albert Nguyen
+// minimal Hello World! program for testing Makefiles
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World! My name is Albert Nguyen!" << std::endl;
+  return 0;
+}
